71a68cbc34d7b88b34aa9e6622e488d972af61e3
yaikeda <yaikeda.io@outlook.com>
2025-05-21 23:53:31 +0900

    2D image assign send and image process in kernel

--- samples/004_pipelineProcess/pipelineProcess.cu
+++ samples/004_pipelineProcess/pipelineProcess.cu
@@ -1,6 +1,21 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <texture_types.h>
+
+__global__ void grayscaleKernel(cudaTextureObject_t texObj, unsigned char* out, int width, int height) // New API can take texObj directly!!!
+{
+    // this is 2D then blockIdx also 2D.
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x >= width || y >= height) return;
+    uchar4 pixel = tex2D<uchar4>(texObj, x, y);
+    unsigned char gray = (pixel.x * pixel.y + pixel.z) / 3;
+    out[y * width + x] = gray;
+}
+
 // Zero fill function
 std::string ZeroPadding(int digits, int num)
 {
@@ -59,6 +74,64 @@ void use_OpenCV(ImageStocker stocker)
     }
 }
 
+void use_SequentialProcess(ImageStocker stocker)
+{
+    for (int i = 0; i < stocker.NumImages(); i++)
+    {
+        // Set Image
+        cv::Mat imageBGRA;
+        cv::cvtColor(stocker.Get(i), imageBGRA, cv::COLOR_BGR2BGRA);
+        cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
+        cudaArray* cuArray;
+        cudaMallocArray(&cuArray, &channelDesc, imageBGRA.cols, imageBGRA.rows); // cudaMallocArray needs image information (like ndarray to Unity Texture2D, row/col/depth info)
+        cudaMemcpy2DToArray(cuArray, 0, 0, // Memcpy to cache. It looks like BufferObject in OpenGL 
+            imageBGRA.ptr<uchar4>(), imageBGRA.step,
+            imageBGRA.cols * sizeof(uchar4), imageBGRA.rows,
+            cudaMemcpyHostToDevice);
+        
+        // Setting Descs
+        cudaResourceDesc resDesc = {};
+        resDesc.resType = cudaResourceTypeArray;
+        resDesc.res.array.array = cuArray;
+        cudaTextureDesc texDesc = {};
+        texDesc.addressMode[0] = cudaAddressModeClamp;
+        texDesc.addressMode[1] = cudaAddressModeClamp;
+        texDesc.filterMode = cudaFilterModePoint;
+        texDesc.readMode = cudaReadModeElementType;
+        texDesc.normalizedCoords = 0;
+
+        // Texture Object
+        cudaTextureObject_t texObj = 0;
+        cudaCreateTextureObject(&texObj, &resDesc, &texDesc, nullptr);
+
+        // Device Memory
+        int width = imageBGRA.cols;
+        int height = imageBGRA.rows;
+        unsigned char* d_output;
+        cudaMalloc(&d_output, width * height * sizeof(unsigned char));
+
+        // Prepare for kernel calling
+        dim3 block(16, 16);
+        dim3 grid((width + 15) / 16, (height + 15) / 16);
+        grayscaleKernel<<<grid, block>>>(texObj, d_output, width, height);
+
+
+        // Save Image
+        cv::Mat outputGray(height, width, CV_8UC1);
+        cudaMemcpy(outputGray.data, d_output, width * height * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+        std::string outpath = "004_cuda-sequential_img_" + ZeroPadding(2, i) + ".png";
+        if (!cv::imwrite(outpath, outputGray)) {
+            std::cerr << "Failed to save image to " << outpath << std::endl;
+            return;
+        }
+        std::cout << "Saved image to: " << outpath << std::endl;
+
+        cudaDestroyTextureObject(texObj);
+        cudaFreeArray(cuArray);
+        cudaFree(d_output);
+    }
+}
+
 // Expected input: a.exe C:\path\to\image\folder num-images
 int main(int argc, char** argv) {
     if (argc < 3)
@@ -69,8 +142,8 @@ int main(int argc, char** argv) {
     std::string imgDir = argv[1];
     size_t imgNum = std::atoi(argv[2]);
     ImageStocker stocker(imgDir, imgNum);
-    std::string baseDir = argv[3];
     use_OpenCV(stocker);
+    use_SequentialProcess(stocker);
 
     // Check images with GUI
     // for (int i = 0; i < stocker.NumImages(); i++)
