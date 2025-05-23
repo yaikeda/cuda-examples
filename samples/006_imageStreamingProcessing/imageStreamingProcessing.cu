b214e0c0a7a6b7bc978f5591449ac6dc925f7728
yaikeda <yaikeda.io@outlook.com>
2025-05-24 00:09:38 +0900

    still sequential

--- samples/006_imageStreamingProcessing/imageStreamingProcessing.cu
+++ samples/006_imageStreamingProcessing/imageStreamingProcessing.cu
@@ -13,67 +13,108 @@ __global__ void grayscaleKernel(cudaTextureObject_t texObj, unsigned char* out, 
     out[y * width + x] = (pixel.x + pixel.y + pixel.z) / 3;
 }
 
-void processSingleImage(ImageStocker& stocker, int index)
+void processSingleImage(ImageStocker& stocker, int index, cudaStream_t stream)
 {
-    // cudaArray
-    cv::Mat imageBGRA;
-    cv::cvtColor(stocker.Get(index), imageBGRA, cv::COLOR_BGR2BGRA);
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
-    cudaArray* cuArray;
-    cudaMallocArray(&cuArray, &channelDesc, imageBGRA.cols, imageBGRA.rows);
-    cudaMemcpy2DToArray(cuArray, // target
-        0, 0, imageBGRA.ptr<char4>(), imageBGRA.step, imageBGRA.cols * sizeof(uchar4), imageBGRA.rows, // image 
-        cudaMemcpyHostToDevice); // direction
 
-    // Texture Object
-    cudaResourceDesc resDesc = {};
-    resDesc.resType = cudaResourceTypeArray;
-    resDesc.res.array.array = cuArray;
-    cudaTextureDesc texDesc = {};
-    texDesc.addressMode[0] = cudaAddressModeClamp;
-    texDesc.addressMode[1] = cudaAddressModeClamp;
-    texDesc.filterMode = cudaFilterModePoint;
-    texDesc.readMode = cudaReadModeElementType;
-    texDesc.normalizedCoords = 0;
-    cudaTextureObject_t texObj = 0;
-    cudaCreateTextureObject(&texObj, &resDesc, &texDesc, nullptr);
-
-    int width = imageBGRA.cols;
-    int height = imageBGRA.rows;
-    unsigned char* d_output;
-    cudaMalloc(&d_output, width * height * sizeof(unsigned char));
-
-    // Kernel call
-    dim3 block(16, 16);
-    dim3 grid((width + 15) / 16, (height + 15) / 16);
-    grayscaleKernel<<<grid, block>>>(texObj, d_output, width, height);
-
-    // Save Image
-    cv::Mat outputGray(height, width, CV_8UC1);
-    cudaMemcpy(outputGray.data, d_output, width * height * sizeof(unsigned char), cudaMemcpyDeviceToHost);
-    std::string outPath = "006_cuda-stream_img_" + ZeroPadding(2, index) + ".png";
-    if (!cv::imwrite(outPath, outputGray))
-    {
-        std::cerr << "Failed to save iamge to " << outPath << std::endl;
-        return ;
-    }
-    std::cout << "Saved image to: " << outPath << std::endl;
-
-    cudaDestroyTextureObject(texObj);
-    cudaFreeArray(cuArray);
-    cudaFree(d_output);
 }
+
+class CudaImageResource {
+    public: 
+        int width;
+        int height;
+        cudaArray* cuArray = nullptr;
+        cudaTextureObject_t texObj = 0;
+        unsigned char* d_output = nullptr;
+        cudaStream_t stream = nullptr;
+        cv::Mat source;
+        cv::Mat output;
+        dim3 block;
+        dim3 grid;
+        void Destroy() {
+            if (texObj != 0) cudaDestroyTextureObject(texObj);
+            if (cuArray != nullptr) cudaFreeArray(cuArray);
+            if (d_output != nullptr) cudaFree(d_output);
+            if (stream != nullptr) cudaStreamDestroy(stream);
+        }
+};
 
 void use_StreamProcess(ImageStocker stocker)
 {
     // Create Stream
-    std::vector<cudaStream_t> streams;
+    std::vector<CudaImageResource> resources;
     int N = stocker.NumImages();
-    streams.resize(N);
+    resources.resize(N);
     for (int i = 0; i < N; i++)
     {
-        cudaStreamCreate(&streams[i]);
-        processSingleImage(stocker, i);
+        cudaStream_t stream;
+        cudaStreamCreate(&stream);
+        resources[i].stream = stream;
+    }
+
+    // Initialize
+    for (int i = 0; i < N; i++)
+    {
+        // cudaArray
+        cv::cvtColor(stocker.Get(i), resources[i].source, cv::COLOR_BGR2BGRA);
+        resources[i].width = resources[i].source.cols;
+        resources[i].height = resources[i].source.rows;
+        cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
+        cudaMallocArray(&resources[i].cuArray, &channelDesc, resources[i].width, resources[i].height);
+
+        // Texture Object
+        cudaResourceDesc resDesc = {};
+        resDesc.resType = cudaResourceTypeArray;
+        resDesc.res.array.array = resources[i].cuArray;
+        cudaTextureDesc texDesc = {};
+        texDesc.addressMode[0] = cudaAddressModeClamp;
+        texDesc.addressMode[1] = cudaAddressModeClamp;
+        texDesc.filterMode = cudaFilterModePoint;
+        texDesc.readMode = cudaReadModeElementType;
+        texDesc.normalizedCoords = 0;
+        cudaCreateTextureObject(&resources[i].texObj, &resDesc, &texDesc, nullptr);
+
+        cudaMalloc(&resources[i].d_output, resources[i].width * resources[i].height * sizeof(unsigned char));
+
+        // Kernel call
+        resources[i].block = dim3(4, 4);
+        resources[i].grid = dim3((resources[i].width + 3) / 4, (resources[i].height + 3) / 4);
+
+        // Save Image
+        resources[i].output = cv::Mat(resources[i].height, resources[i].width, CV_8UC1);
+    }
+
+    // Set Tasks
+    for (int i = 0; i < N; i++)
+    {
+        cudaMemcpy2DToArrayAsync(resources[i].cuArray, // target
+            0, 0, resources[i].source.ptr<uchar4>(), resources[i].source.step, resources[i].width * sizeof(uchar4), resources[i].height, // image 
+            cudaMemcpyHostToDevice, resources[i].stream); // direction
+    }
+
+        for (int i = 0; i < N; i++)
+    {
+        grayscaleKernel<<<resources[i].grid, resources[i].block, 0, resources[i].stream>>>(resources[i].texObj, resources[i].d_output, resources[i].width, resources[i].height);
+    }
+        for (int i = 0; i < N; i++)
+    {
+        cudaMemcpyAsync(resources[i].output.data, resources[i].d_output, resources[i].width * resources[i].height * sizeof(unsigned char), cudaMemcpyDeviceToHost, resources[i].stream);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cudaStreamSynchronize(resources[i].stream);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        std::string outPath = "006_cuda-stream_img_" + ZeroPadding(2, i) + ".png";
+        if (!cv::imwrite(outPath, resources[i].output))
+        {
+            std::cerr << "Failed to save iamge to " << outPath << std::endl;
+            return ;
+        }
+        std::cout << "Saved image to: " << outPath << std::endl;
+        resources[i].Destroy();
     }
 }
 
